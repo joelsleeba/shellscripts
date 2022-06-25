58b86610c403e95b1341ef73b86ce2703b20dcb9
joelsleeba <joelsleeba1@gmail.com>
2022-06-25 21:40:45 +0530

    dash-to-dock
    Signed-off-by: joelsleeba <joelsleeba1@gmail.com>

---
+++ gsettings-dash-to-dock.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide-mode 'ALL_WINDOWS'
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide-in-fullscreen true
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'DYNAMIC'
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
