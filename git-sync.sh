a5c8bd43557fee6fcc36c74c061396479a5a0216
Joel Sleeba <54855387+joelsleeba@users.noreply.github.com>
2022-08-16 23:22:53 +0530

    Create git-sync.sh

---
+++ git-sync.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Generate commit message automatically
+
+gstatus=`git status --porcelain`
+
+if [ ${#gstatus} -ne 0 ]
+
+then
+
+ git add --all
+
+ # sleep 10 seconds
+
+ sleep 10
+
+ git commit -m "$gstatus"
+
+ sleep 10
+
+ git pull --rebase
+
+ sleep 10
+
+ git push
+
+fi
