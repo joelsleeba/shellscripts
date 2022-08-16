5c51d294b836850c7a42b4bc0529599ca54ce847
Joel Sleeba <54855387+joelsleeba@users.noreply.github.com>
2022-08-16 19:10:11 +0530

    Update vncclient.sh

--- vncclient.sh
+++ vncclient.sh
@@ -3,16 +3,19 @@ then
     mkdir $HOME/.ssh
 fi
 
-# change this to your ssh private key
-curl https://raw.githubusercontent.com/joelsleeba/shellscripts/master/vipervnc > $HOME/.ssh/vipervnc
+KEYFILE=$HOME/.ssh/vipervnc
 
-echo "Enter ip address of remote machine:"
+# change this to your ssh private key
+curl -s https://raw.githubusercontent.com/joelsleeba/shellscripts/master/vipervnc > $KEYFILE
+chmod 600 $KEYFILE
+
+echo -n "Enter ip address of remote machine:"
 read ssh_remote
 
-if [[ ssh_remote == *":"* ]]
+if [[ $ssh_remote==*":"* ]]
 then
     ssh_remote_ip=$(echo $ssh_remote|sed -E 's/(([0-9]{1,3}\.){3}[0-9]{1,3}):([0-9]{1,4})/\1/g')
-    ssh_remote_ip=$(echo $ssh_remote|sed -E 's/(([0-9]{1,3}\.){3}[0-9]{1,3}):([0-9]{1,4})/\3/g')
+    ssh_remote_port=$(echo $ssh_remote|sed -E 's/(([0-9]{1,3}\.){3}[0-9]{1,3}):([0-9]{1,4})/\3/g')
 else
     ssh_remote_ip=$ssh_remote
     ssh_remote_port=22
