7159411980bcf9eccdf008daba74fee35eb3bd19
joelsleeba <joelsleeba@gmail.com>
2022-08-22 15:45:20 +0530

    changed hardcoded path to relative

--- install-git-local.sh
+++ install-git-local.sh
@@ -2,6 +2,6 @@ wget -c https://github.com/git/git/archive/refs/heads/master.zip -O $HOME/Downlo
 unzip $HOME/Downloads/git.zip -d $HOME/Downloads
 cd $HOME/Downloads/git-master
 make configure
-./configure --prefix=/home/user/.local
-make prefix=/home/user/.local all doc info
-make prefix=/home/user/.local install
+./configure --prefix=$HOME/.local
+make prefix=$HOME/.local all doc info
+make prefix=$HOME/.local install
