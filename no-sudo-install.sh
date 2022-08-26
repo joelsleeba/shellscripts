d50e8f6171e1ff43fc1f8a8e9ed9d17b29690a3a
joelsleeba <joelsleeba1@gmail.com>
2022-08-26 16:16:49 +0530

    added a combined install file

---
+++ no-sudo-install.sh
@@ -0,0 +1,10 @@
+cd install
+./install-zsh-local.sh
+./install-git-local.sh
+./install-pip3-local.sh
+./install-build-local.sh
+./install-texlive-local.sh
+./install-foliate-local.sh
+./install-go-local.sh
+./install-hugo-local.sh
+
