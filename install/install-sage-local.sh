a5ebd266616a58028c31a95c828bb3d3839a05eb
joelsleeba <joelsleeba1@gmail.com>
2022-08-29 14:20:28 +0530

    sagemath install bug fix
    Signed-off-by: joelsleeba <joelsleeba1@gmail.com>

--- install/install-sage-local.sh
+++ install/install-sage-local.sh
@@ -5,15 +5,17 @@ if [[ ! $(conda --version) ]]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
     chmod +x *conda*
     bash Miniconda*
+    source $HOME/.bashrc
     if [[ $(zsh --version) ]]; then
         conda init zsh
+	source $HOME/.zshrc
     fi
 fi
 
 conda config --set auto_activate_base false
-
-conda install mamba
-
+conda update conda
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+conda install -c conda-forge mamba
 mamba create -n sage-env sage jupyterlab python=3.10
-
 conda activate sage-env
